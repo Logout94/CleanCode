9a45783961f0555ec5839fe871cf5fa6dfdd4309
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-11-27 20:02:04 +0900

    refact: solution 참고하여 수정. rect, my_rect 같이 불명확한 네이밍을 명확한 rectangle 명사로 변경, CoordX와 같이 중복성이 있는 것을 그냥 X으로 변경

--- 1-naming/01-challenge-problem.py
+++ 1-naming/01-challenge-problem.py
@@ -1,39 +1,39 @@
 class Point:
-    def __init__(self, coordX, coordY):
-        self.coordX = coordX
-        self.coordY = coordY
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
 
 
 class Rectangle:
     def __init__(self, top_left_point, width, height):
         self.top_left_point = top_left_point
-        self.top_right_X = 0
-        self.bottom_left_Y = 0
+        self.top_right_x = 0
+        self.bottom_left_y = 0
         self.width = width
         self.height = height
         self.set_end_point()
 
-    def area(self):
+    def get_area(self):
         return self.width * self.height
 
     def set_end_point(self):
-        self.top_right_X = self.top_left_point.coordX + self.width
-        self.bottom_left_Y = self.top_left_point.coordY + self.height
+        self.top_right_x = self.top_left_point.x + self.width
+        self.bottom_left_y = self.top_left_point.y + self.height
 
     def print(self):
-        print('Starting Point (X)): ' + str(self.top_left_point.coordX))
-        print('Starting Point (Y)): ' + str(self.top_left_point.coordY))
-        print('End Point X-Axis (Top Right): ' + str(self.top_right_X))
-        print('End Point Y-Axis (Bottom Left): ' + str(self.bottom_left_Y))
-        print('Area : ' + str(self.area()))
+        print('Starting Point (X)): ' + str(self.top_left_point.x))
+        print('Starting Point (Y)): ' + str(self.top_left_point.y))
+        print('End Point X-Axis (Top Right): ' + str(self.top_right_x))
+        print('End Point Y-Axis (Bottom Left): ' + str(self.bottom_left_y))
+        print('Area : ' + str(self.get_area()))
 
 
-def get_rect(top_left_X, top_left_Y):
-    top_left_point = Point(top_left_X, top_left_Y)
-    rect = Rectangle(top_left_point, 90, 10)
+def get_rectangle(top_left_x, top_left_y):
+    top_left_point = Point(top_left_x, top_left_y)
+    rectangle = Rectangle(top_left_point, 90, 10)
 
-    return rect
+    return rectangle
 
 
-my_rect = get_rect(50, 100)
-my_rect.print()
+user_rectangle = get_rectangle(50, 100)
+user_rectangle.print()
