ce58adfc9d21a61ab0d42175eb1e86bc4d08d37a
Dokyu Lee <leewan@Dokyuui-MacBookPro.local>
2022-11-27 19:39:04 +0900

    refact: 좌표와 X, Y 라벨을 명확히함

--- 1-naming/01-challenge-problem.py
+++ 1-naming/01-challenge-problem.py
@@ -5,10 +5,10 @@ class Point:
 
 
 class Rectangle:
-    def __init__(self, top_left, width, height):
-        self.top_left = top_left
-        self.top_right = 0
-        self.bottom_left = 0
+    def __init__(self, top_left_point, width, height):
+        self.top_left_point = top_left_point
+        self.top_right_X = 0
+        self.bottom_left_Y = 0
         self.width = width
         self.height = height
         self.set_end_point()
@@ -17,20 +17,20 @@ class Rectangle:
         return self.width * self.height
 
     def set_end_point(self):
-        self.top_right = self.top_left.coordX + self.width
-        self.bottom_left = self.top_left.coordY + self.height
+        self.top_right_X = self.top_left_point.coordX + self.width
+        self.bottom_left_Y = self.top_left_point.coordY + self.height
 
     def print(self):
-        print('Starting Point (X)): ' + str(self.top_left.coordX))
-        print('Starting Point (Y)): ' + str(self.top_left.coordY))
-        print('End Point X-Axis (Top Right): ' + str(self.top_right))
-        print('End Point Y-Axis (Bottom Left): ' + str(self.bottom_left))
+        print('Starting Point (X)): ' + str(self.top_left_point.coordX))
+        print('Starting Point (Y)): ' + str(self.top_left_point.coordY))
+        print('End Point X-Axis (Top Right): ' + str(self.top_right_X))
+        print('End Point Y-Axis (Bottom Left): ' + str(self.bottom_left_Y))
         print('Area : ' + str(self.area()))
 
 
 def get_rect(top_left_X, top_left_Y):
-    top_left = Point(top_left_X, top_left_Y)
-    rect = Rectangle(top_left, 90, 10)
+    top_left_point = Point(top_left_X, top_left_Y)
+    rect = Rectangle(top_left_point, 90, 10)
 
     return rect
 
